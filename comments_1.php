818cfdd2c30769c3f4a06c5c9ff83c0462b291ed
Radinal <radinallsnr@gmail.com>
2022-09-28 15:34:32 +0700

    28092022_comments

---
+++ comments_1.php
@@ -0,0 +1,5 @@
+<?php
+echo "<p>hallo dunia!</p>";
+echo "<p>saya belajar php</p>";
+echo "<p>ini program pertama saya</p>";
+?>
---
+++ comments_2.php
@@ -0,0 +1,5 @@
+<?php
+echo "saya belaajr php\n";
+echo "ini baris kedua\n";
+echo "ini baris ketiga\n";
+?>
